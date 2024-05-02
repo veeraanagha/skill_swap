c5cb1c990972bc402e943f3e15ef1a96acc5e5a7
kodaKodama <KodaKodama@users.noreply.github.com>
2024-05-02 11:17:45 -0700

    creat user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const skillSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    skills: [skillSchema],
+    interests: [skillSchema],
+    matchRequests: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    matches: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+}, {timestamps: true})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
