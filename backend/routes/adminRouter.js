545d534ff1ae9cd79dd12cbe0b4838b797df2afe
Himanshu-Lilhore <himanshulilhore@gmail.com>
2024-05-05 19:02:23 +0530

    minor changes

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -4,7 +4,7 @@ const adminController = require('../controllers/adminController')
 
 router.get('/users', adminController.showAllUsers)
 router.get('/skills', adminController.showAllSkills)
-router.post('/add/skill', adminController.addSkill)  // single or a list objects
-router.post('/add/user', adminController.addUser)   // single or a list objects
+router.post('/add/skill', adminController.addSkill)  // single or a list of objects
+router.post('/add/user', adminController.addUser)   // single or a list of objects
 
 module.exports = router
