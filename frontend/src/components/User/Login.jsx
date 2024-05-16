9fd5f82560996cd0ee2203ed4ad951f1f6e8ed0b
wellitsabhi <abhishekongit@gmail.com>
2024-05-16 23:02:31 +0530

    autocomplete password implemented

--- frontend/src/components/User/Login.jsx
+++ frontend/src/components/User/Login.jsx
@@ -55,7 +55,7 @@ const Login = () => {
                 />
             </div>
             <div className="mb-5">
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label htmlFor="password" aria-autocomplete='' className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Your password
                 </label>
                 <input
